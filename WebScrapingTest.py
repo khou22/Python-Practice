e401375a8786df2d99e21290a480aded8894cc1c
Kevin Hou <kevin.ch.hou@gmail.com>
2016-01-30 13:24:27 -0800

    Experimenting with Web Scraping

--- WebScrapingTest.py
+++ WebScrapingTest.py
@@ -2,10 +2,10 @@ from bs4 import BeautifulSoup
 import lxml
 import urllib2
 
-response = urllib2.urlopen("https://en.wikipedia.org/wiki/Beautiful_Soup") #Get markdown
+response = urllib2.urlopen("https://login.yahoo.com/config/login?.src=fpctx&.intl=us&.lang=en-US&.done=https%3A%2F%2Fwww.yahoo.com") #Get markdown
 
 html = response.read() #Markdown
 
 soup = BeautifulSoup(html, "lxml") # Using lxml parser
 print(soup.title)
-print(soup.body.findAll(text="Python"))
+print(soup.body)
